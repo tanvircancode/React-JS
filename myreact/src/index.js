8b804edd2805c491f42ad74d9d9277c510a01c41
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-09-20 14:04:42 +0600

    up

--- myreact/src/index.js
+++ myreact/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const index = 0;
-const element = (
-    <h1 className="heading" tabIndex={index}>
-        <span className="text">Hello World</span>
-        <img src="" />
-    </h1>
-);
-ReactDOM.render(element, document.querySelector('#root'));
+
+setInterval(() => {
+    const element = (
+
+        <h1 className="heading" tabIndex={index}>
+            <span className="text">Hello {new Date().toLocaleTimeString()}</span>
+        </h1>
+    );
+    ReactDOM.render(element, document.querySelector('#root'));
+}, 1000);
