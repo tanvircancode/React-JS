68ca23e78a8aa6f19886860805a6de10889c9ddd
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-09-22 13:11:30 +0600

    updated

---
+++ myreact/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const index = 0;
+
+
+setInterval(() => {
+    const element = (
+        <h1 className="heading" tabIndex={index}>
+            <span className="text">Hello {new Date().toLocaleTimeString()}</span>
+
+        </h1>
+    );
+
+    ReactDOM.render(element, document.getElementById('root'));
+},1000);
