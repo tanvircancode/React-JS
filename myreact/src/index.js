0b87fb2b89e588b20359d6e1383884f785c89db8
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-09-23 13:08:51 +0600

    updated

--- myreact/src/index.js
+++ myreact/src/index.js
@@ -1,16 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 
-const index = 0;
-
-
-setInterval(() => {
-    const element = (
-        <h1 className="heading" tabIndex={index}>
-            <span className="text">Hello {new Date().toLocaleTimeString()}</span>
-
-        </h1>
+function Clock({locale}) {
+    return (
+      <h1 className="heading">
+        <span className="text">{new Date().toLocaleTimeString(locale)}</span>
+      </h1>
     );
+}
 
-    ReactDOM.render(element, document.getElementById('root'));
-},1000);
+ReactDOM.render(<Clock locale="bn-BD" />, document.getElementById('root'));
