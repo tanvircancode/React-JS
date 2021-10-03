423d1236b4da5d9337bfd33a79ed2d4e826906b5
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-10-03 20:41:21 +0600

    updated

--- myreact/src/components/Clock.js
+++ myreact/src/components/Clock.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 
 
 class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { date: new Date() };
-//     }
+  constructor(props) {
+    super(props);
+      this.state = { date: new Date(), locale: 'bn-BD' };
+      this.handleClick = this.handleClick.bind(this);
+    }
 
-    state = { date: new Date() };
-    
+    // state = { date: new Date(), locale: 'bn-BD' };
+
     componentDidMount() {
        this.clockTimer = setInterval(() =>  this.tick(), 1000);
     }
@@ -18,20 +19,32 @@ class Clock extends React.Component {
         clearInterval(this.clockTimer);
     }
 
+    handleClick()  {
+        this.setState({
+          locale: 'en-US',
+        })
+    }
+
     tick() {
         this.setState({
           date: new Date(),
         });
     }
 
-  render() {
-    return (
-      <h1 className="heading">
-        <span className="text">
-          {this.state.date.toLocaleTimeString(this.props.locale)}
-        </span>
-      </h1>
-    );
+    render() {
+        const { date,locale } = this.state;
+      return (
+        <div>
+          <h1 className="heading">
+            <span className="text">
+              {date.toLocaleTimeString(locale)}
+            </span>
+          </h1>
+          <button type="button" onClick={this.handleClick}>
+            Click here
+          </button>
+        </div>
+      );
   }
 }
 
